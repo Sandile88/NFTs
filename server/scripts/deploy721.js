b1c2693a87bc7b9f76b05102663548163231b9d4
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-11-10 02:11:32 +0200

    added ERC721 deploy script

---
+++ server/scripts/deploy721.js
@@ -0,0 +1,22 @@
+const hre = require("hardhat");
+
+async function main() {
+    const SimpleERC721A = await hre.ethers.getContractFactory("SimpleERC721A");
+    const nft = await SimpleERC721A.deploy(
+        "Picture",
+        "pic",
+        "QmXzwvrKWut84q6fVbebHQdJddM6DHrCAAbfJa84TAonVU/",
+        2
+    );
+
+    await nft.waitForDeployment();
+    const address = await nft.getAddress();
+    console.log("NFT contract deployed to: ", address); 
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
