69a6cb8c5a787acfd75b4d685d430c2b80c5f874
Sandile M'remi <sm-remi023@student.wethinkcode.co.za>
2024-11-12 16:58:47 +0200

    added simple frontend and abi to see minted nfts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Web3 from 'web3';
+import { useState } from 'react';
+import token from "../../server/jsons/erc721a.json"
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [contract, setContract] = useState(null);
+  const [balance, setBalance] = useState(0);
+  const [mintingInProgress, setMintingInProgress] = useState(false);
+
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
+
+        const accounts = await web3Instance.eth.requestAccounts();
+        setAccounts(accounts);
+
+        const contractAddress = "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266";
+        const instance = new web3Instance.eth.Contract(token, contractAddress);
+        setContract(instance);
+
+        const balance = await instance.methods.balanceOf(accounts[0]).call(); //getting balance
+        setBalance(balance);
+  
+
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.error('Web3 not found');
+    }
+  };
+
+
+  const mintToken = async () => {
+    if (contract && accounts.length > 0) {
+      try {
+        setMintingInProgress(true);
+        await contract.methods.mint(1).send({ from: accounts[0] });
+        setMintingInProgress(false);
+        console.log("NFT minted successfully");
+
+        // trying to get updated balance
+        const balance = await contract.methods.balanceOf(accounts[0]).call();
+        setBalance(balance);
+        
+      } catch (error) {
+        setMintingInProgress(false);
+        console.log("Minting failed: ", error);
+        
+      }
+    }
+  }
 
   return (
-    <>
+    <div>
+    <h1>Vending Machine App</h1>
+    {web3 ? (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <p>Connected: {accounts[0]}</p>
+        <p>NFT Balance: {balance}</p>
+        <button onClick={mintToken} disabled={mintingInProgress}>
+          {mintingInProgress ? "Minting in progress..." : "Mint NFT"}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    ) : (
+      <button onClick={connectWallet}>Connect Wallet</button>
+    )}
+
+    <div></div>
+  </div>
   )
 }
 
--- server/contracts/SimpleERC721A.sol
+++ server/contracts/SimpleERC721A.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// import "@openzeppelin/contracts/token/ERC1155/ERC721.sol";
 import "erc721a/contracts/ERC721A.sol";
+import "hardhat/console.sol";
 
 contract SimpleERC721A is ERC721A {
-    //is it possible to make the state variables below into a struct?
     string public baseURI;
     uint256 public maxSupply;
     uint256 public tokenCounter;
 
-    constructor (
+    constructor(
         string memory _name,
         string memory _symbol,
         string memory _baseURI,
@@ -19,18 +20,18 @@ contract SimpleERC721A is ERC721A {
         maxSupply = _maxSupply;
     }
 
-    function mintToken(uint256 quantity) external {
-        require(totalSupply() + quantity <= maxSupply, "Exceeds maximum supply");
+    function mint(uint256 quantity) external {
+        require(totalSupply() + quantity <= maxSupply, "Exceeds max supply");
         _safeMint(msg.sender, quantity);
+        
     }
 
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(_exists(tokenId), "Token doesn't exist");
+        require(_exists(tokenId), "Token does not exist");
         return string(abi.encodePacked(baseURI, _toString(tokenId), ".png"));
     }
 
-    //get all minted NFTs
-    function getNfts(uint256 counter) public returns (uint256) {
-        return tokenCounter+= counter;
+    function getNfts(uint256 counter) public returns(uint256) {
+        return tokenCounter = counter;
     }
 }
